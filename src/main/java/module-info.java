b1bf88b5fdb6beec79b0da86577160b8ff69bfdf
Sophie NABITZ <sophie.nabitz@univ-avignon.fr>
2020-11-30 11:55:26 +0100

    Version initiale du projet

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+open module td {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+}
---
+++ src/test/java/composite/ControleurTest.java
@@ -0,0 +1,13 @@
+package composite;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControleurTest {
+
+    @Test
+    void shouldbeRenamedAndImplemented() {
+    }
+
+}
